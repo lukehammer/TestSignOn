6218b26979438d132b3935ac688277f54eb10809
Luke Hammer <Luke Hammer@94L7ZF2-PC>
2017-08-29 12:57:47 -0700

    Add project files.

---
+++ TestSignOn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestSignOn.Startup))]
+namespace TestSignOn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
